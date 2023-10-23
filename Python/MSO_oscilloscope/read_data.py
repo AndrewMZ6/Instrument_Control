e0997fdb706eda1e2a066691d04c4c4b5d14bcd5
AndrewMZ6 <andreyzh322@gmail.com>
2023-10-23 12:18:19 +0700

    python test read data

--- Python/MSO_oscilloscope/read_data.py
+++ Python/MSO_oscilloscope/read_data.py
@@ -29,62 +29,39 @@ def find_indexes(data: np.ndarray, sign: str, yref: int) -> np.ndarray:
     return indexes
                 
                 
-osci = rm.open_resource(T[1])
-print(osci.query("*IDN?"))
-print(osci.query("*IDN?"))
-print(osci.query("*IDN?"))
-print(osci.query("*IDN?"))
+with rm.open_resource(T[1]) as osci:
+    print(osci.query("*IDN?"))
 
 
-osci.timeout = 10000
-osci.write(':STOP')
+    osci.timeout = 10000
+    osci.write(':STOP')
 
-osci.write(':WAV:SOUR CHAN1')
-osci.write(':WAV:MODE RAW')
+    osci.write(':WAV:SOUR CHAN1')
+    osci.write(':WAV:MODE RAW')
 
-osci.write(':WAV:FORM BYTE')
-osci.write(':WAV:POINts 160000')
-
-
-pre = list(map(lambda x: float(x), osci.query(':WAV:PRE?').split(',')))
-
-data = osci.query_binary_values(':WAV:DATA?', datatype='B', container=np.ndarray)
-
-err = osci.query(':SYST:ERR?')
-osci.close()
-data = np.array(data, dtype=int)
+    osci.write(':WAV:FORM BYTE')
+    osci.write(':WAV:POINts 3000000')
 
 
+    pre = list(map(lambda x: float(x), osci.query(':WAV:PRE?').split(',')))
+    print(f'{pre=}')
+
+    try:
+
+        data = osci.query_binary_values(':WAV:DATA?', datatype='B', container=np.ndarray)
+        print(data[:10])
+
+    except e:
+        print(e)
+
+    err = osci.query(':SYST:ERR?')
+    osci.write(':RUN')
+    #osci.close()
+    data = np.array(data, dtype=int)
+    print(f'{len(data) = }')
 
 
-yincrement = pre[7]
-yref = int(pre[9])
-xincrement = pre[4]
 
-
-revived_sig = np.zeros(len(data), dtype=float)
-ypositive_indexes = find_indexes(data, '>', yref)
-ynegative_indexes = find_indexes(data, '<', yref)
-
-positive_data = (data[ypositive_indexes] - yref)*yincrement
-negative_data = (data[ynegative_indexes] - yref)*yincrement
-
-revived_sig[ypositive_indexes] = positive_data
-revived_sig[ynegative_indexes] = negative_data
-
-
-fs = 25e6
-fc = 5e6
-interpolated_size = 1024*20
-freqline = np.arange(0, fs - 1, fs/interpolated_size)
-t = np.arange(0, len(revived_sig)*(1/fs), 1/fs)
-        
-Q_carr = -np.sin(2*np.pi*fc*t)
-I_carr = np.cos(2*np.pi*fc*t)
-
-real_part = np.multiply(revived_sig, I_carr)
-imag_part = np.multiply(revived_sig, Q_carr)
-
-
-plt.plot(np.abs(np.fft.fft(real_part)))
+plt.plot(data)
 plt.grid()
+plt.show()
